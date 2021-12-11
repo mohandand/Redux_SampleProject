3602aeddea078fa5d3697ed6bb3ec43f37ea0ae8
mohandand <mohandand@gmail.com>
2021-12-10 16:30:35 -0800

    updated code

--- redux_sample_project/src/index.js
+++ redux_sample_project/src/index.js
@@ -26,7 +26,12 @@ const decrement = () => {
 //REDUCER
 
 const counter = (state=0,action) => {
-  switch(action.type)
+  switch(action.type){
+    case 'INCREMENT':
+    return state + 1;
+    case 'DECREMENT':
+    return state + 1;
+  }
 
 }
 
