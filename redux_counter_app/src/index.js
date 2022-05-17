7b63c418b0bf265f4b4bd57bd1976aa7b46f5cb3
mohandand <mohandand@gmail.com>
2022-05-16 22:56:02 -0700

    Updating Code

--- redux_counter_app/src/index.js
+++ redux_counter_app/src/index.js
@@ -3,40 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from 'redux';
-
-let store = createStore(reducer);
-
-//STORE -> GLOBALIZED STATE
-
-//ACTION INCREMENT
-
-const increment = () => {
-  return {
-    type: "INCREMENT",
-
-  }
-}
-const decrement = () => {
-  return {
-    type: "DECREMENT",
-
-  }
-}
-//REDUCER
-
-const counter = (state=0,action) => {
-  switch(action.type){
-    case 'INCREMENT':
-    return state + 1;
-    case 'DECREMENT':
-    return state + 1;
-  }
-
-}
-
-//DISPATCH
-
 
 ReactDOM.render(
   <React.StrictMode>
